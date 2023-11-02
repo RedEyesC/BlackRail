4cf8a97bc1532563cd98e608b4630c871c7f31ac
RedEyesC <1875077877@qq.com>
2023-11-02 17:00:30 +0800

    修改默认配置

--- Assets/Editor/RecastEditor/RecastConfig.cs
+++ Assets/Editor/RecastEditor/RecastConfig.cs
@@ -9,7 +9,7 @@ namespace GameEditor.RecastEditor
         public static readonly string MapElement = "MapElement";
 
         //构建寻路网格的参数
-        public static readonly int MAX_HEIGHT = 100;
+        public static readonly int MAX_HEIGHT = 500;
         public static readonly int RC_NOT_CONNECTED = 0x3f;//空心高度场，相邻不可行走标志，RecastConfig.RC_NOT_CONNECTED-1 为最大层级
         public static readonly int MAX_LAYERS = RC_NOT_CONNECTED - 1;
 
@@ -19,22 +19,22 @@ namespace GameEditor.RecastEditor
         public static readonly int RC_CONTOUR_REG_MASK = 0xffff;
 
         public static readonly float AgentMaxSlope = 45;
-        public static readonly float AgentMaxClimb = 3f;
+        public static readonly float AgentMaxClimb = 0.9f;
         public static readonly float AgentHeight = 2.0f;
-        public static readonly float AgentRadius = 1f;
-        public static readonly float CellSize = 0.5f; //xz平面的尺寸
-        public static readonly float CellHeight = 0.5f; // y轴的尺寸
+        public static readonly float AgentRadius = 0.6f;
+        public static readonly float CellSize = 0.3f; //xz平面的尺寸
+        public static readonly float CellHeight = 0.2f; // y轴的尺寸
 
-        public static readonly float MinRegionArea = 10; //小于此则被剔除
+        public static readonly float MinRegionArea = 8; //小于此则被剔除
         public static readonly float MergeRegionArea = 20; //小于此则与周围区域合并
 
-        public static readonly float MaxSimplificationError = 1; //最大简化误差 ，大于则视为简化点
+        public static readonly float MaxSimplificationError = 1.3f; //最大简化误差 ，大于则视为简化点
         public static readonly bool TessellateWallEdges = true;//是否根据最大边缘长度细化不可行走的边缘
         public static readonly bool TessellateAreaEdges = true;//是否根据最大边缘长度是否细化区域之间的边缘
         public static readonly float MaxEdgeLen = 12; //最大边缘长度
 
-        public static readonly bool FilterLowHangingObstacles = false;//过滤悬空可走的span
-        public static readonly bool FilterLedgeSpans = false;//过滤高度差过大的span
-        public static readonly bool FilterWalkableLowHeightSpans = false;//过滤不可通过的高度的span
+        public static readonly bool FilterLowHangingObstacles = true;//过滤悬空可走的span
+        public static readonly bool FilterLedgeSpans = true;//过滤高度差过大的span
+        public static readonly bool FilterWalkableLowHeightSpans = true;//过滤不可通过的高度的span
     }
 }
