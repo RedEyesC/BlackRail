195c0a6aabae811bcf7866cc86e25913591ee040
RedEyesC <1875077877@qq.com>
2024-03-18 17:11:57 +0800

    补充一个简易的打包脚本

---
+++ Assets/Editor/AssetBuilder/AssetBuilderConfig.cs
@@ -0,0 +1,56 @@
+﻿using UnityEditor;
+
+namespace GameEditor.AssetBuidler
+{
+    public enum BuildPlatform
+    {
+        Win,
+        Android,
+        IOS,
+    };
+
+
+    public class BuildPlatformConfig
+    {
+        public string platformName;
+        public string buildLocationPathName;
+        public string assetTargetPathEx = "";
+        public string pluginPath;
+        public string appTargetPath;
+        public string[] defaultScene = { "Assets/Resources/Init/Default.unity" };
+        public ScriptingImplementation scriptingImplementation;
+        public BuildTargetGroup buildTargetGroup;
+        public BuildPlatform buildPlatform;
+        public BuildTarget buildTarget;
+        public BuildOptions buildOption = BuildOptions.StrictMode | BuildOptions.CompressWithLz4HC;
+        public BuildAssetBundleOptions buildAssetBundleOptions;
+
+        
+
+    }
+
+    public static class AssetBuilderConfig
+    {
+        private static BuildPlatformConfig[] mBuildConfig;
+
+        static AssetBuilderConfig()
+        {
+            mBuildConfig = new BuildPlatformConfig[3];
+
+            mBuildConfig[(int)BuildPlatform.Win] = new BuildPlatformConfig();
+            mBuildConfig[(int)BuildPlatform.Win].platformName = "win";
+            mBuildConfig[(int)BuildPlatform.Win].buildLocationPathName = "C:/Users/admin/Desktop/tool/tool.exe";
+            mBuildConfig[(int)BuildPlatform.Win].buildPlatform = BuildPlatform.Win;
+            mBuildConfig[(int)BuildPlatform.Win].buildTargetGroup = BuildTargetGroup.Standalone;
+            mBuildConfig[(int)BuildPlatform.Win].buildTarget = BuildTarget.StandaloneWindows64;
+            mBuildConfig[(int)BuildPlatform.Win].scriptingImplementation = ScriptingImplementation.IL2CPP;
+            mBuildConfig[(int)BuildPlatform.Win].buildAssetBundleOptions = BuildAssetBundleOptions.ChunkBasedCompression | BuildAssetBundleOptions.AppendHashToAssetBundleName | BuildAssetBundleOptions.DeterministicAssetBundle;
+
+        }
+
+        public static BuildPlatformConfig GetConfig(BuildPlatform platform)
+        {
+            return mBuildConfig[(int)platform];
+        }
+    }
+}
---
+++ Assets/Editor/AssetBuilder/AssetBuilderCtrl.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+
+namespace GameEditor.AssetBuidler
+{
+    public static class AssetBuilderCtrl
+    {
+
+
+        #region Build Asset
+
+        [MenuItem("Assets/Game Editor/BuildPlayer", true)]
+        static bool ValidBuildPlayer()
+        {
+            return true;
+        }
+
+
+        //简易的打包脚本，需要手动把res需要的资源移到对应路径下
+        [MenuItem("Assets/Game Editor/BuildPlayer", false, 900)]
+        public static void BuildPlayer()
+        {
+            BuildPlatformConfig config = AssetBuilderConfig.GetConfig(BuildPlatform.Win);
+
+            PlayerSettings.companyName = "BlackRail";
+            PlayerSettings.productName = "BlackRail";
+            PlayerSettings.colorSpace = ColorSpace.Gamma;
+
+            PlayerSettings.SetScriptingBackend(config.buildTargetGroup, config.scriptingImplementation);
+
+            PlayerSettings.fullScreenMode = FullScreenMode.Windowed;
+            PlayerSettings.defaultScreenWidth = 1280;
+            PlayerSettings.defaultScreenHeight = 720;
+
+            BuildPipeline.BuildPlayer(config.defaultScene, config.buildLocationPathName, config.buildTarget, config.buildOption);
+
+        }
+        #endregion
+    }
+}
