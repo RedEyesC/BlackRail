a1dd58e4bb38624f5c3d9831bd02350ade6c3aaa
RedEyesC <1875077877@qq.com>
2024-04-18 16:45:42 +0800

    补充通用模块

--- Assets/Engine/Core/Common/GameModule.cs
+++ Assets/Engine/Core/Common/GameModule.cs
@@ -15,11 +15,6 @@ namespace GameFramework.Runtime
         public abstract void Start();
 
 
-        /// <summary>
-        /// 游戏框架模块轮询。
-        /// </summary>
-        /// <param name="elapseSeconds">逻辑流逝时间，以秒为单位。</param>
-        /// <param name="realElapseSeconds">真实流逝时间，以秒为单位。</param>
         public abstract void Update(float elapseSeconds, float realElapseSeconds);
 
 
