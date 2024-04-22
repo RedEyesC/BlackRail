8fac424e7c24cc2d213c0b69538becf526269849
RedEyesC <1875077877@qq.com>
2024-04-22 21:23:21 +0800

    重命名ui，接入ingamedebugconsole插件

--- Assets/Engine/Core/AppLoopManager/AppLoopStart.cs
+++ Assets/Engine/Core/AppLoopManager/AppLoopStart.cs
@@ -1,4 +1,6 @@
-﻿namespace GameFramework.Runtime
+﻿using UnityEngine;
+
+namespace GameFramework.Runtime
 {
     public class AppLoopStart : StateBase
     {
@@ -10,13 +12,31 @@
         public override void StateUpdate(float elapseSeconds, float realElapseSeconds)
         {
             GameCenter.Update(elapseSeconds, realElapseSeconds);
-
         }
 
         public override void StateEnter(params object[] paramList)
         {
+            InitInGameDebugConsole();
+
             GameCenter.CreateInstance();
             GameCenter.Start();
         }
+
+        public void InitInGameDebugConsole()
+        {
+
+            GameObject log = new GameObject("Logger");
+            log.SetParent(GameObject.Find("_AppRoot"), false);
+
+            string bundleName = "UI/Console.ab";
+            AssetManager.LoadAllAssetAsync(bundleName, (Request request) =>
+            {
+                GameObject obj = AssetManager.GetAssetObjWithType<GameObject>(bundleName, "IngameDebugConsole", true);
+                GameObject go = GameObject.Instantiate<GameObject>(obj);
+                go.SetParent(log,false);
+            });
+
+        }
+
     }
 }
