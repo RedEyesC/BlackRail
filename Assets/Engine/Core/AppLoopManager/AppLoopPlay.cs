7747c110d7040dd4730ab59db57070d12a7cbad8
RedEyesC <12875077877@qq.com>
2024-10-30 22:50:28 +0800

    调整相机

--- Assets/Engine/Core/AppLoopManager/AppLoopPlay.cs
+++ Assets/Engine/Core/AppLoopManager/AppLoopPlay.cs
@@ -16,7 +16,7 @@ namespace GameFramework.AppLoop
             Role mainRole = SceneCtrl.GetMainRole();
 
             //// 远景 28，50，35，0，0，中景 20，50，35，0，0， 近景 10，60，20，0，0   
-            SceneManager.SetTarget(mainRole.root, 10f, 35, 20, 0, 0);
+            SceneManager.SetTarget(mainRole.root, 15f, 35, 25, 0, 0);
 
             mainRole.SetPosition(-66,499);
             mainRole.PlayAnim("Idle");
