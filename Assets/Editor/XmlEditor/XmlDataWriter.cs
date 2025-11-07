c14519f4834f70434c603c7d1af4b75cf1e56abb
RedEyesC <1875077877@qq.com>
2025-11-07 15:24:16 +0800

    优化xml导出

--- Assets/Editor/XmlEditor/XmlDataWriter.cs
+++ Assets/Editor/XmlEditor/XmlDataWriter.cs
@@ -1,25 +1,25 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Xml.Linq;
 using System.Xml.Serialization;
 
 namespace GameEditor.XmlEditor
 {
     public class XmlDataWriter
     {
-
-
-        //TODO 单条数据的时候，表格不好看
-        public static void SaveListToXml<T>(List<T> list, string filePath) where T : new()
+        public static void SaveListToXml<T>(List<T> list, string filePath)
+            where T : new()
         {
+            //单条数据的时候，wps打开表格不好看，添加一个空对象
+            if (list.Count == 1)
+            {
+                list.Add(new T());
+            }
+
             XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
             using (FileStream fs = new FileStream(filePath, FileMode.Create))
             {
                 serializer.Serialize(fs, list);
             }
         }
-
     }
 }
