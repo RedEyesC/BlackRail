87381ab04b53ba89a259978e060639fc10afed22
RedEyesC <1875077877@qq.com>
2026-06-04 16:38:24 +0800

    动作编辑器初版提交

---
+++ Assets/Editor/ActionEditor/Editor/View/ActionAssetInspector.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ActionEditor
+{
+    [CustomEditor(typeof(ActionImporter))]
+    internal class ActionAssetInspector : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            EditorGUILayout.Space(20);
+
+            if (GUILayout.Button("Open Action Editor"))
+            {
+                if (target != null)
+                {
+                    Selection.activeObject = target as UnityEngine.Object;
+                }
+
+                ActionEditorWindow.OpenDirectorWindow();
+            }
+        }
+    }
+}
---
+++ Assets/Editor/ActionEditor/Editor/View/ActionEditorWindow.cs
@@ -0,0 +1,16 @@
+﻿using TrackEditor;
+
+namespace ActionEditor
+{
+    public class ActionEditorWindow : TrackEditorWindow
+    {
+        public static void OpenDirectorWindow()
+        {
+            var window = GetWindow(typeof(ActionEditorWindow)) as ActionEditorWindow;
+            if (window == null)
+                return;
+            window.InitializeAll();
+            window.Show();
+        }
+    }
+}
